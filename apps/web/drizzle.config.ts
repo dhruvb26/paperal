4a77bf47d753d8428d911685909f3f64546a4815
Kanav Gupta <kanavg004@gmail.com>
2025-07-13 02:13:40 -0700

    chore: fix drizzle error

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -7,13 +7,6 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    database: 'postgres',
-    port: 5432,
-    host: 'aws-0-us-west-1.pooler.supabase.com',
-    password: env.DATABASE_PASSWORD,
-    user: env.DATABASE_USER,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    url: env.DATABASE_URL,
   },
 })
--- apps/web/src/db/index.ts
+++ apps/web/src/db/index.ts
@@ -11,7 +11,15 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined
 }
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL)
+const conn = globalForDb.conn ?? postgres(env.DATABASE_URL, {
+  ssl: { rejectUnauthorized: false },
+  connection: {
+    options: `--search_path=public`,
+  },
+  max: 10, // Maximum number of connections
+  idle_timeout: 20, // Close connections after 20 seconds of inactivity
+  connect_timeout: 10, // Connection timeout in seconds
+})
 if (env.NODE_ENV !== 'production') globalForDb.conn = conn
 
 export const db = drizzle(conn, { schema })
